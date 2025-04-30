1f7a95cefc14e306633c6e8a42fe418a84fbe055
ratiogamo <127904893+ratiogamo@users.noreply.github.com>
2025-04-30 16:34:48 +0000

    netlify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip.tsx";
-import NotFound from "@/pages/not-found.tsx";
-import Home from "@/pages/Home.tsx";
-import MainLayout from "@/components/MainLayout.tsx";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import NotFound from "./pages/not-found";
+import Home from "./pages/Home";
+import MainLayout from "./components/MainLayout";
 function Router() {
   return (
     <Switch>
