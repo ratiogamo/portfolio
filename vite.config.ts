6524eb6c8d2d871a174232b7b20715351f5f9e5b
ratiogamo <127904893+ratiogamo@users.noreply.github.com>
2025-04-30 19:15:40 +0000

    deploy fix2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ssr from 'vite-plugin-ssr/plugin'
 import path from "path";
 export default defineConfig({
   base: './',
   plugins: [
     react(),
+    ssr(),
   ],
   resolve: {
     alias: {
