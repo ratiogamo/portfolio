feb95afe11892c4f49e4056633fc6aac6850dd51
ratiogamo <ratiogamo@gmail.com>
2025-09-28 18:54:11 -0400

    [dyad] Adjusted vertical padding for Automation Cube to be consistent with other sections - wrote 1 file(s)

--- client/src/components/AutomationCube.tsx
+++ client/src/components/AutomationCube.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AutomationCube: React.FC = () => {
   return (
-    <section className="py-10"> {/* Changed from py-16 to py-10 */}
+    <section className="py-16"> {/* Changed from py-10 to py-16 for consistent vertical padding */}
       <div className="container mx-auto px-4">
         <div className="flex justify-center items-center">
           <div className="glass-card p-6 rounded-2xl">
